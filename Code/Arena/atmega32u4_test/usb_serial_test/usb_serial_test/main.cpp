83cc6de1b4a28e3baa5efe483b7cf90651e0f564
creamytoad <mathias.lervik@hotmail.com>
2024-01-24 09:52:26 +0100

    added code folder

---
+++ Code/Arena/atmega32u4_test/usb_serial_test/usb_serial_test/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * usb_serial_test.cpp
+ *
+ * Created: 24/01/2024 09:51:57
+ * Author : mathi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
