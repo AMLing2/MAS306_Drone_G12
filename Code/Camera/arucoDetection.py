3df06f10ea6f043461060cf06e2576dc5fe5a3e5
Thomas <ThomaL2002@hotmail.com>
2024-01-30 14:47:11 +0100

    Update ArUco script: colors, ID text++

--- Code/Camera/arucoDetection.py
+++ Code/Camera/arucoDetection.py
@@ -1,16 +1,27 @@
+# PyImageSearch: https://pyimagesearch.com/2020/12/21/detecting-aruco-markers-with-opencv-and-python/
 
+# --------------------------------------- Libraries ---------------------------------------
 import cv2
 import cv2.aruco as aruco # For simplification
 import pyrealsense2 as rs
 import numpy
+# --------------------------------------- Libraries ---------------------------------------
 
+# ------------------- Constant variables for simple changes -------------------
+markerColor = (255, 255, 0) # Corner color is set to be contrast to border color
 
-# Set dictionary for the markers
-arucoParams     = aruco.DetectorParameters()
-arucoDictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_1000) # Tips from GPT, Detector_get is old
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontColor = (200, 20, 200)
+fontScale = 0.5
+fontThickness = 1
 
 vectorRotation    = None
 vectorTranslation = None
+# ------------------- Constant variables for simple changes -------------------
+
+# Set dictionary for the markers
+arucoParams     = aruco.DetectorParameters()
+arucoDictionary = aruco.getPredefinedDictionary(aruco.DICT_6X6_1000) # <-- Tip from chatGPT, Detector_get is old
 
 
 # Setup configuration and start pipeline stream
@@ -20,28 +31,50 @@ config = rs.config()
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30) # (streamType, xRes, yRes, format, fps)
 pipe.start(config)
 
+
 while(True):
     
-
-    frame = pipe.wait_for_frames() # waits for and collects all frames from camera (depth, color, etc)
-    color_frame = frame.get_color_frame()
-    color_image = numpy.asanyarray(color_frame.get_data())
+    # Frame Collection
+    frame = pipe.wait_for_frames()          # collect frames from camera (depth, color, etc)
+    color_frame = frame.get_color_frame()   # Extract RGB module frame
+    color_image = numpy.asanyarray(color_frame.get_data()) # Convert to NumPy array
 
     # Identification
-    gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY) # Grayscale image
+    gray = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)   # Grayscale image
     corners, ids, rejectedImagePoints = aruco.detectMarkers(gray, arucoDictionary, parameters=arucoParams)
 
-    # Draw outline of markers
-    color_image = aruco.drawDetectedMarkers(color_image, corners, borderColor=(0,0,255))
+    # Information display in terminal
+    print(f'\n[INFO] ArUco Marker ID:     {ids}') # Array with current IDs: [ [id1] [id2] ... [idn] ]
+    print('[INFO] Corners: ')
+    print(corners)  # Matrix with current corner coordinates [ x1, y1; x2, y2; x3, y3; x4, y4 ], [-||-], ...
 
 
-    cv2.imshow('LiveReading', color_image)     # Display the current frame
-    print('ArUco Marker ID: ')
-    print(ids)
-    print('Corners: ')
-    print(corners)      # Matrix with coordinates of all 4 corners[ x1, y1; x2, y2; x3, y3; x4, y4 ]
+    # Display outline of markers
+    color_image = aruco.drawDetectedMarkers(color_image, corners, borderColor=markerColor)
 
-    if cv2.waitKey(1) == ord('q'):
+    # --------------------- v Display ID of marker, by pyImageSearch v ---------------------
+    if len(corners) > 0:    # So iteration makes sense
+
+        for (markerCorner, markerID) in zip(corners, ids): # alias on left, so can iterate in parallel
+            # Extract corners
+            corners = markerCorner.reshape(4,2)
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight    = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft  = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft     = (int(topLeft[0]), int(topLeft[1]))
+
+            # Display on the image
+            cv2.putText(color_image, str(ids),(topLeft[0], topLeft[1] - 15), font, fontScale, fontColor, fontThickness)
+    # --------------------- ^ Display ID of marker, by pyImageSearch ^ ---------------------
+            
+    # Display image
+    cv2.imshow('LiveReading', color_image)
+
+    # Loop breaker
+    if cv2.waitKey(1) == ord('q'): # Press Q to stop
         break
 
 pipe.stop()             # Stop recording
