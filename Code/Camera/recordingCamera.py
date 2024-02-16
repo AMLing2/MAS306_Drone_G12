9380130dada653512b17d032524f1c607d0b76cb
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-02-16 15:29:29 +0100

    Revised recording without bag files

--- Code/Camera/recordingCamera.py
+++ Code/Camera/recordingCamera.py
@@ -1,40 +1,53 @@
-# ---- In order to numerate the videos, a solution provided by ezro at StackOverflow was used ----
-# https://stackoverflow.com/questions/72329662/how-to-keep-cv2-imwrite-from-overwriting-the-image-files
-
-import cv2
-import pyrealsense2 as rs
-import numpy
+import cv2                  # OpenCV
+import pyrealsense2 as rs   # RealSense Wrapper
+import numpy                # Python Math
 import os                   # For path
 
+# ------------------ Recording Setup ------------------
 # Directory to save video 
 dir = r'/home/thomaz/Recordings'
 os.chdir(dir)
 
-# Size of window to display recording
-displaySize = (960, 540)
+# Four-Character Code (File format)
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+
+# Image Transfer Rate
+fps = 60
+
+# Image size: (horizontal, vertical) [pixels]
+size = (848, 480)
+
+# File Configuraiton for recording
+recording = cv2.VideoWriter('recording.avi', fourcc, fps, size)
+# ------------------ Recording Setup ------------------
 
 # Depth Camera connection
 pipe = rs.pipeline()
 cfg = rs.config()
 
-cfg.enable_stream(rs.stream.color, 1920, 1080, rs.format.bgr8, 30) # (streamType, xRes, yRes, format, fps)
-cfg.enable_record_to_file('recordedVideo.bag')
-
+# Setup Stream
+cfg.enable_stream(rs.stream.color, size, rs.format.bgr8, fps) # (streamType, xRes, yRes, format, fps)
 pipe.start(cfg)
 
 while(True):
-    
+    # Extract and convert frame
     frame = pipe.wait_for_frames() # waits for and collects all frames from camera (depth, color, etc)
     color_frame = frame.get_color_frame()
     color_image = numpy.asanyarray(color_frame.get_data())
 
-    displayWindow = cv2.resize(color_image, displaySize)
+    # Save frame to file
+    recording.write(color_image)
 
-    cv2.imshow('LiveReading', displayWindow)     # Display the current frame
+    # In case of too large size to display
+    #color_image = cv2.resize(color_image, displaySize)
+
+    # Display the current frame
+    cv2.imshow('LiveReading', color_image)
 
     # Press Q to stop recording
     if cv2.waitKey(1) == ord('q'):
         break
 
+recording.release()
 pipe.stop()             # Stop recording
 cv2.destroyAllWindows() # Free resources
