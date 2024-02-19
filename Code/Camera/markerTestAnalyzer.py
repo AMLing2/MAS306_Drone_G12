be6fa29716e1cf609bdcb7ca28c08bab1ea287a8
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-02-19 13:06:55 +0100

    Cleaner code

--- Code/Camera/markerTestAnalyzer.py
+++ Code/Camera/markerTestAnalyzer.py
@@ -1,6 +1,3 @@
-# https://www.geeksforgeeks.org/how-to-get-the-magnitude-of-a-vector-in-numpy/
-
-# --------------------------------------- Libraries ---------------------------------------
 import pyrealsense2 as rs   # stream configuration
 import cv2                  # Show video with OpenCV
 import cv2.aruco as aruco   # Simplification
@@ -8,10 +5,6 @@ import os                   # Check file-extension
 import numpy                # Python Math
 import csv                  # Export to "Comma-Separated Values" file
 import time                 # Computation Timer
-import math                 # For Square Root
-# --------------------------------------- Libraries ---------------------------------------
-
-#markerColor = (255, 255, 0) # Corner color is set to be contrast to border color
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 fontColor = (200, 20, 200)
@@ -66,14 +59,6 @@ computationTime = []    # Array for computation times
 # False positives detection tolerance - Change with empirical testing
 falseTol = 0.085
 
-# Function from GeeksForGeeks, see link in top
-def magnitude(vector): 
-    # Convert vector to a NumPy array if it's not already one
-    vector = numpy.array(vector)
-    
-    # Compute magnitude using NumPy's linear algebra norm function
-    return numpy.linalg.norm(vector)
-
 # Export data to csv file
     # Path and name of file
 csvFile = r'/home/thomaz/MAS306_Drone_G12/Code/Camera/Results/markerTestResults.csv'
@@ -87,8 +72,6 @@ with open(csvFile, 'w', newline='') as file:
     csvwriter.writerow(['Dictionary', 'Rotation Vector', 'Translation Vector'])
 
     for dict in dictList:
-        
-        print(dict)
 
         # Restart variables for relevant values
         startTimer = time.time() # Start timer per dict
@@ -127,38 +110,18 @@ with open(csvFile, 'w', newline='') as file:
                     rotVector, transVector, markerPoints = cv2.aruco.estimatePoseSingleMarkers(
                         corners[i], markerSize, cameraMatrix=cameraMatrix, distCoeffs=distortionCoefficients)
 
-                    # Check for false positives
-                    # if ( ( (prevRotVec - rotVector) > falseTol ).any() or
-                    #      ( (prevTransVec - transVector) > falseTol ).any() ):
-                    #     curFalsePos += 1
-
-                    if ( ( (magnitude(prevTransVec) - magnitude(transVector)) > falseTol) ): #or
-                        #  ( (magnitude(prevRotVec) - magnitude(rotVector)) > falseTol) ):
+                    # Check for false positives - change in magnitude (Vector Norm |x|)
+                    if ( ( (numpy.linalg.norm(prevTransVec) - numpy.linalg.norm(transVector)) > falseTol) ):
                         curFalsePos += 1
-                        print("False Positive detected!")
-                    else:
-                        print("No False pos.")
 
                     prevRotVec = rotVector
                     prevTransVec = transVector
-
-                        # This may be redundant if we detect false positives with too big difference between frames
-                    # if (len(ids) > 1):
-                    #     print("More than 1 marker")
-
-                    # Draw axes
-                    # cv2.drawFrameAxes(frame, cameraMatrix=cameraMatrix,
-                    #                 distCoeffs=distortionCoefficients, rvec=rotVector, tvec=transVector, length=axesLength)
                 
                 # Number of frames with data
                 curDataFrames += 1
 
             # Write to CSV file
             csvwriter.writerow([dict, rotVector, transVector])
-
-            # Display Video
-            #displayWindow = cv2.resize(frame, displaySize)    # Resize window
-            #cv2.imshow('LiveReading', displayWindow)                # Display the current frame
         
             # Press Q to stop video playback
             if cv2.waitKey(1) == ord('q'):
