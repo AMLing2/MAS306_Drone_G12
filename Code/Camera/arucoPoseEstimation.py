187d94b778bbc53c9bcac6aab8faff774955c71b
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-02-19 18:24:51 +0100

    Added updated arucoPoseEstimate pre-multi marker trackin

--- Code/Camera/arucoPoseEstimation.py
+++ Code/Camera/arucoPoseEstimation.py
@@ -16,13 +16,20 @@ fontScale = 0.5
 fontThickness = 1
 axesLength = 0.01
 
-markerSize = 0.04 # Length of ArUco marker sides [m]
+markerSize = 0.051 # Length of ArUco marker sides [m]
 
-# Imported from Camera Calibration, hardcoded for testing first
+# Imported from Camera Calibration
+    # For 848x480
 cameraMatrix = numpy.array([
-    [1379.333496,   0,         973.144470],     # [f_x, 0.0, c_x] used principal points 
-    [  0,         1378.661255,  535.200500],    # [0.0, f_y, c_y] as optical center points
-    [  0,           0,           1.0      ] ])  # [0.0, 0.0, 1.0]
+    [613.037048339844,          0,         429.841949462891],    # [f_x, 0.0, c_x] used principal points 
+    [  0,               612.738342285156,  237.866897583008],    # [0.0, f_y, c_y] as optical center points
+    [  0,                       0,                1.0      ] ])  # [0.0, 0.0, 1.0]
+    # For 1920x1080
+# cameraMatrix = numpy.array([
+#     [1379.333496,   0,         973.144470],     # [f_x, 0.0, c_x] used principal points 
+#     [  0,         1378.661255,  535.200500],    # [0.0, f_y, c_y] as optical center points
+#     [  0,           0,           1.0      ] ])  # [0.0, 0.0, 1.0]
+
 distortionCoefficients = numpy.array(
     [ 0.0, 0.0, 0.0, 0.0, 0.0]) # [k1, k2, p1, p2, k3]
 
@@ -39,10 +46,10 @@ arucoDictionary = aruco.getPredefinedDictionary(aruco.DICT_7X7_50) # <-- Tip fro
 pipe = rs.pipeline()
 config = rs.config()
 
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30) # (streamType, xRes, yRes, format, fps)
+config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 60) # (streamType, xRes, yRes, format, fps)
 pipe.start(config)
 
-
+transVector = [0, 0, 0]
 while(True):
     
     # Frame Collection
@@ -63,13 +70,18 @@ while(True):
             
             cv2.drawFrameAxes(color_image, cameraMatrix=cameraMatrix,
                               distCoeffs=distortionCoefficients, rvec=rotVector, tvec=transVector, length=axesLength)
+            
+            magnitude = numpy.linalg.norm(transVector)
+            magnitude = round(magnitude, 4)
+            print("\nMagnitude: ", magnitude)
     # ------------------------------------ Pose Estimation ------------------------------------
 
 
     # Information display in terminal
-    print(f'\n[INFO] ArUco Marker ID:     {ids}') # Array with current IDs: [ [id1] [id2] ... [idn] ]
-    print('[INFO] Corners: ')
-    print(corners)  # Matrix with current corner coordinates [ x1, y1; x2, y2; x3, y3; x4, y4 ], [-||-], ...
+    # print(f'\n[INFO] ArUco Marker ID:     {ids}') # Array with current IDs: [ [id1] [id2] ... [idn] ]
+    # print('[INFO] Corners: ')
+    # print(corners)  # Matrix with current corner coordinates [ x1, y1; x2, y2; x3, y3; x4, y4 ], [-||-], ...
+
 
     # --------------------- v Display ID of marker, by pyImageSearch v ---------------------
     if len(corners) > 0:    # So iteration makes sense
