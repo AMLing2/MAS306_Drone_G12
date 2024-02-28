6f6ae53808c483350f202253ad23fc78d582e854
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-02-28 11:30:46 +0100

    Added working C++ code with cmake in VS Code. Both OpenCV and Librealsense (realsense in other file)

---
+++ Code/Camera/Cpp/build/CMakeFiles/opencvTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencvTest.dir/poseMarkerDepth.cpp.o"
+  "CMakeFiles/opencvTest.dir/poseMarkerDepth.cpp.o.d"
+  "opencvTest"
+  "opencvTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencvTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Code/Camera/Cpp/poseMarkerDepth.cpp
@@ -0,0 +1,16 @@
+
+#include <librealsense2/rs.hpp> // Import RealSense Library
+#include <iostream>             // Standard IO
+
+#include <opencv2/opencv.hpp>
+
+int main() {
+
+
+    rs2::pipeline pipe;
+    rs2::config config;
+
+    std::cout << "\nIt is dfgdfgdgfd!\n" << std::endl;
+
+    return 0;
+}
