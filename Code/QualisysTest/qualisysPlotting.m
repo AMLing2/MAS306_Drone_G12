09a3ee1ab9ce1ade0336cad1f4aabf63ae268215
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-04-16 18:41:57 +0200

    MATLAB plotting prettified

--- Code/QualisysTest/qualisysPlotting.m
+++ Code/QualisysTest/qualisysPlotting.m
@@ -2,7 +2,7 @@ clc; clear; close all;
 
 %% Plotting results Qualisys Test 
 
-data = csvread("ExportedResults_12.csv", 2,0);
+data = csvread("ExportedResults_0.csv", 2,0);
 
 time = data(:,1);
 xQTM = data(:,2);
@@ -18,30 +18,39 @@ figure(Name="Plot of Translation comparison")
 % X plotting
 sgtitle("Translation Comparison")
 subplot(3,1,1)
-plot(time,xQTM)
+plot(time,xQTM, '.r')
 hold on
-plot(time,xCV)
-legend('xQTM', 'xCV')
+plot(time,xCV, '.k', MarkerSize=1)
 ylabel('x [m]')
 xlabel('Time [seconds]')
+[a, icons] = legend('xQTM', 'xCV', 'Location','eastoutside');
+% Change size of legend icons
+icons = findobj(icons, '-property', 'Marker', '-and', '-not', 'Marker', 'none');
+set(icons, 'MarkerSize', 20)
 
 % Y plotting - Remember to flip sign, only axis which is aligned
 subplot(3,1,2)
-plot(time,yQTM)
+plot(time,yQTM, '.g')
 hold on
-plot(time,-yCV)
-legend('yQTM', 'yCV')
+plot(time,-yCV, '.k', MarkerSize=1)
 ylabel('y [m]')
 xlabel('Time [seconds]')
+[a, icons] = legend('yQTM', 'yCV', 'Location','eastoutside');
+% Change size of legend icons
+icons = findobj(icons, '-property', 'Marker', '-and', '-not', 'Marker', 'none');
+set(icons, 'MarkerSize', 20)
 
 % Z plotting
 subplot(3,1,3)
-plot(time,zQTM)
+plot(time,zQTM, '.b')
 hold on
-plot(time,zCV)
-legend('zQTM', 'zCV')
+plot(time,zCV, '.k', MarkerSize=1)
 ylabel('z [m]')
 xlabel('Time [seconds]')
+[a, icons] = legend('zQTM', 'zCV', 'Location','eastoutside');
+% Change size of legend icons
+icons = findobj(icons, '-property', 'Marker', '-and', '-not', 'Marker', 'none');
+set(icons, 'MarkerSize', 20)
 
 %%%%%%%%%%% Rotation Plotting %%%%%%%%%%%
 
@@ -55,20 +64,35 @@ for i = 1:9
     vec0 = data(:, i+7); % v0Rij
     QTM = data(:, i+25); % v1Rij
     
-    if any(i == [3, 6, 7, 8])
-        plot(time, abs(vec0))
+    if any(i == [1,4,7])
+        plot(time, vec0, '.r')
+    elseif any(i == [2,5,8])
+        plot(time, vec0, '.g')
     else
-        plot(time, vec0)
+        plot(time, vec0, '.b')
     end
-        hold on;
-        plot(time, QTM)
+    hold on;
+    plot(time, QTM, '.k', MarkerSize=1)
     
     xlabel('Time [seconds]');
-    %ylabel(['Value of v' num2str(floor((i-1)/3)+1) 'R' num2str(mod(i-1,3)+1)])
     title(['Element ', num2str(i)])
-    legend('Vec0', 'QTM');
     hold off;
+    xlim([12.9 27])
 end
+
+% Add a bit space to the figure
+fig = gcf;
+fig.Position(3) = fig.Position(3) + 250;
+
+% Add common legend outside subplots
+[lgd, icons] = legend('Vec0', 'QTM');
+lgd.Position(1) = 0.01;
+lgd.Position(2) = 0.4;
+
+% Change size of legend icons
+icons = findobj(icons, '-property', 'Marker', '-and', '-not', 'Marker', 'none');
+set(icons, 'MarkerSize', 20)
+
 
 figure(Name="Rotation Comparison1")
 sgtitle("Rotation Matrix Comparison: Vec1 and QTM")
@@ -80,13 +104,31 @@ for i = 1:9
     vec1 = data(:, i+16); % v0Rij
     QTM = data(:, i+25); % v1Rij
     
-    plot(time, vec0)
+    if any(i == [1,4,7])
+        plot(time, vec1, '.r')
+    elseif any(i == [2,5,8])
+        plot(time, vec1, '.g')
+    else
+        plot(time, vec1, '.b')
+    end
     hold on;
-    plot(time, QTM)
+    plot(time, QTM, '.k', MarkerSize=1)
     
     xlabel('Time [seconds]');
-    %ylabel(['Value of v' num2str(floor((i-1)/3)+1) 'R' num2str(mod(i-1,3)+1)])
     title(['Element ', num2str(i)])
-    legend('Vec1', 'QTM');
     hold off;
+    xlim([12.9 27])
 end
+
+% Add a bit space to the figure
+fig = gcf;
+fig.Position(3) = fig.Position(3) + 250;
+
+% Add common legend outside subplots
+[lgd, icons] = legend('Vec1', 'QTM');
+lgd.Position(1) = 0.01;
+lgd.Position(2) = 0.4;
+
+% Change size of legend icons
+icons = findobj(icons, '-property', 'Marker', '-and', '-not', 'Marker', 'none');
+set(icons, 'MarkerSize', 20)
