ed950553433f5c3fe64243f3bf10d3d998384d34
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-04-19 09:01:13 +0200

    Cleaned folder, also added export of transVector1 at the end of list. Renamed poseTestAnalysis.py to poseTestSynchExport.py to reflect functio

--- Code/QualisysTest/poseTestSynchExport.py
+++ Code/QualisysTest/poseTestSynchExport.py
@@ -6,24 +6,14 @@ import numpy                # Python Math
 import csv                  # Data import
 # --------------------------------------- Libraries ---------------------------------------
 
-# ------------------ Import/Export Setup ------------------
 # Directory to save video 
 dir = r'/home/thomaz/poseValidationTest'
 os.chdir(dir)
 
 # Test number for easy change
-testNr = 4
+testNr = 0
 
 tolerance = 0.1 # meters
-
-# Plotting variables
-#timeQTMplot = []
-#timeCVplot = []
-#timePlot = []
-#xQTM,yQTM,zQTM = [],[],[]
-#xCV,yCV,zCV    = [],[],[]
-
-# ------------------ Import/Export Setup ------------------
 
 rotVectors = []
 transVectors = []
@@ -64,7 +54,7 @@ transQTM = [row[5:8] for row in QTMdata[14:]]
 transQTM = numpy.array(transQTM, dtype=float)/1000.0 # millimeters -> meters
 
 # Measured physical distances from RGB camera to Qualisys L-Frame
-pTransVec = numpy.array([0.192, 0.144, 1.564])   # meters
+pTransVec = numpy.array([0.190, 0.143, 1.564])   # meters
 pTransArr = numpy.tile(pTransVec, (len(transQTM), 1))
 
 # Correct translation
@@ -140,10 +130,10 @@ for row in rotElementsQTM:
 
 # Setup csv file
 filename = f"ExportedResults_{testNr}.csv" 
-fields = ['Time','xQTM', 'yQTM', 'zQTM', 'xCV', 'yCV', 'zCV', 
+fields = ['Time','xQTM', 'yQTM', 'zQTM', 'xCV0', 'yCV0', 'zCV0',
           'v0R11','v0R12','v0R13','v0R21','v0R22','v0R23','v0R31','v0R32','v0R33',
           'v1R11','v1R12','v1R13','v1R21','v1R22','v1R23','v1R31','v1R32','v1R33',
-          'qtmR11','qtmR12','qtmR13','qtmR21','qtmR22','qtmR23','qtmR31','qtmR32','qtmR33']
+          'qtmR11','qtmR12','qtmR13','qtmR21','qtmR22','qtmR23','qtmR31','qtmR32','qtmR33', 'xCV1', 'yCV1', 'zCV1',]
 
 # Column names
 with open(filename, 'w') as csvfile:
@@ -169,9 +159,6 @@ with open(filename,'a') as csvfile:
         rotVector0 = numpy.array(dataOpenCV[loopRound+1][8:11], dtype=numpy.float32)
         rotVector1 = numpy.array(dataOpenCV[loopRound+1][11:14], dtype=numpy.float32)
 
-        #print("\ntransVector0: ", transVector0)
-        #print("\nrotVector0: ", rotVector0)
-
         # Save current magnitude
         curTransMag = numpy.linalg.norm(transVector0)
 
@@ -180,10 +167,6 @@ with open(filename,'a') as csvfile:
             #print("loopRound: ", loopRound)
             startTime = timestampOpenCV[loopRound]
             startLoop = loopRound
-            #startIter = iterOpenCV[loopRound-1]
-            #print("\nLoopRound: ", loopRound)
-            #print("startIter: ", startIter)
-            #print("startTime: ", startTime)
             detectedStart = True
         
         # Save last 10 frames magnitude
@@ -200,36 +183,13 @@ with open(filename,'a') as csvfile:
                 if (0 < ((timeQTM[i] - startTimeQTM) - curTime)):
                     correctTimeQTM = timeQTM[i]
                     break
-
-            #print("\nCurrent Time: ", curTime)
-            #print("\nOpenCV Time: ", timestampOpenCV[loopRound])
-            #print("\nQTM Time: ", correctTimeQTM)
-
-            #print("\nRotVector: ", rotVectors[0])
-            #print("\nRotVectorElement: ", rotVectors[0][1][0])
             
-            #print("\nrotmatQTM: ", rotMatQTM[i])
-            #print("\nrotmatQTMElement: ", rotMatQTM[i][0][1])
             rotMat0, _ = cv2.Rodrigues(rotVector0)
             rotMat1, _ = cv2.Rodrigues(rotVector1)
             
-#                if not ((transVector0[0] == 0.0) and (transVector0[1] == 0.0) and (transVector0[2] == 0.0)):
-#                    rotMat0, _ = cv2.Rodrigues(rotVector0)
-#                    #print("\nRotMat0: ", rotMat0)
-#                else:
-#                    rotMat0 = numpy.zeros((3, 3), dtype=numpy.float32)
-#                if not ((transVector1[0] == 0.0) and (transVector1[1] == 0.0) and (transVector1[2] == 0.0)):
-#                    rotMat1, _ = cv2.Rodrigues(rotVector1)
-#                else:
-#                    rotMat1 = numpy.zeros((3, 3), dtype=numpy.float32)
-
-        #print("\nRotMat0Element: ", rotMat0[1][2]) # [row][column]
-
-            #print("\ni for csvwriter QTM: ", i)
             # Export time and translation vectors
             csvwriter.writerow([curTime, transQTM[i][0], transQTM[i][1], transQTM[i][2],
                                 transVector0[0], transVector0[1], transVector0[2],
-                                #transVector1[0], transVector1[1], transVector1[2],
                                 rotMat0[0][0], rotMat0[0][1], rotMat0[0][2],
                                 rotMat0[1][0], rotMat0[1][1], rotMat0[1][2],
                                 rotMat0[2][0], rotMat0[2][1], rotMat0[2][2],
@@ -238,13 +198,12 @@ with open(filename,'a') as csvfile:
                                 rotMat1[0][2], rotMat1[2][1], rotMat1[2][2], 
                                 rotMatQTM[i][0][0], rotMatQTM[i][0][1], rotMatQTM[i][0][2],
                                 rotMatQTM[i][1][0], rotMatQTM[i][1][1], rotMatQTM[i][1][2],
-                                rotMatQTM[i][2][0], rotMatQTM[i][2][1], rotMatQTM[i][2][2],])
-
-        ########################## THIS IS DONE INSIDE DETECTION #########################
-        #rounds.append(loopRound)
+                                rotMatQTM[i][2][0], rotMatQTM[i][2][1], rotMatQTM[i][2][2],
+                                transVector1[0], transVector1[1], transVector1[2],])
+            # transVector1 was added later, hence it's last placement to avoid plot script redesign
+            
+            ########################## THIS IS DONE INSIDE DETECTION #########################
 
 print("\nStartOpen: ", startTime)
 print("\nStartQTM: ", startTimeQTM)
 print("\nStartLoop: ", startLoop)
-
-#print("\nLoops: ", rounds)
