8fc3c79b54f73e58731d6903a231564100d8be4d
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-04-26 15:15:17 +0200

    Cleaned code, removed pre-interpolation

--- Code/KalmanFilter/KalmanFilter1D.m
+++ Code/KalmanFilter/KalmanFilter1D.m
@@ -18,12 +18,6 @@ trans = [0.190 0.143 1.564]; % Physically measured in m
 
 %% Interpolate QTM data for more consistent values
 
-% for i = 1 : length(zQTM)
-%     if zQTM(i) == 0
-%         zQTM(i) = z
-%     end
-% end
-
 % New timeseries with constant period
 Hz = 100;               % [samples/second]
 dt = 1/Hz;              % [seconds]
@@ -35,15 +29,8 @@ zQTMi = zQTM;
 zQTMi(nanIdx) = NaN;
 % zQTMiSpline = zQTMi;
 
-% Find the indices of the NaN values in the position list
-nan_idx = find(isnan(zQTMi));
-
-% Interpolate the missing values in the position list using the time values
-zQTMi =       interp1(time(~isnan(zQTMi)), zQTMi(~isnan(zQTMi)), t);
-% zQTMiSpline = interp1(time(~isnan(zQTMiSpline)), zQTMi(~isnan(zQTMiSpline)), t, 'spline');
-
-% Interpolate NaN values
-% zQTMi = interp1(time, zQTMi,time);
+% Interpolation: (oldTime, interpBetween, newTime)
+zQTMi = interp1(time(~isnan(zQTMi)), zQTMi(~isnan(zQTMi)), t);
 
 %%% Plotting Interpolation %%%
 
@@ -73,19 +60,10 @@ Q = 10; % Covariance Matrix
 % Measurement Noise v_n
 R = 0.1; % Covariance Matrix
 
-% one = [1; 2];
-% two = eye(2);
-% three = one*two*one'
-
-% dt = time(2)-time(1);
-% B = [0 dt];
-% % B = [B Vd 0*B];
-% P = B*Q*B';
-
 %%%%%%%%%%%%%%%%%%%% Translation Plotting %%%%%%%%%%%%%%%%%%%%
 % startTime = 2.5;
 % stopTime = 2.5;
-startTime = 10;
+startTime = 0;
 stopTime = time(end);
 
 transPlot = figure(Name="Plot of Translation comparison");
@@ -99,88 +77,66 @@ plot(t,zQTMi, '.k', MarkerSize=1)
 
 
 %%%%%%%%%%%%%%%%%%%% Translation Plotting %%%%%%%%%%%%%%%%%%%%
-% iter = 1;
 
-timeTol = 0.1;
-yPrev = zCV0(1);
+% Initialization
+timeTol = 0.01;     % Sync low sps to high sps
+yPrev = zCV0(1);    % Start measurement
+x = [ zQTM(1); 0];  % Start [z, zDot]
 
-x = [ zQTM(1); 0];
+% u = (zQTM(i) - zQTM(i-1))/(dt^2);          % [m/s^2]
+% x = [ zQTMi(i); (zQTMi(i) - zQTMi(i-1))/(dt) ];
 
+% Preallocate Space
 zKalman = zeros(length(t),1);
+
+% State Transition Matrix
+A = [ 1   dt ;
+      0   1 ];
+% Control Input Matrix
+B = [ dt^2  ;
+      dt   ];
+% Covariance?
+P = B*Q*B';
 
 % Time-Varying Kalman Filter
 for i = 2 : length(t)
-    % if (zQTM(i) ~= trans(3))
-       
-        % curTime = time(i);                             % [seconds]
 
-        % State Transition Matrix
-        A = [ 1   dt ;
-              0   1 ];
-        % Control Input Matrix
-        B = [ dt^2  ;
-              dt ];
-
-        % Find closest value
-        for CViter = 1 : length(time)
-            if (abs(time(CViter) - t(i)) < timeTol)
-                y = zCV0(CViter);
-                break
-            elseif (i ~= 1)
-                y = yPrev;
-            end
+    % Find closest value
+    for CViter = 1 : length(time)
+        if (abs(time(CViter) - t(i)) < timeTol)
+            y = zCV0(CViter);
+            break
+        elseif (i ~= 1)
+            y = yPrev;
         end
-        % CViter = 1;
-        % if (time(CViter))
-            % Closest 
-        % end
+    end
 
-        % Initialization
-        if i == 2
-            % dt = time(i) - time(i-1);                  % [seconds]
-            % B = [dt^2/2; dt];
-            P = B*Q*B';
-            % Acceleration
-            % u = (zQTM(i) - zQTM(i-1))/(dt^2);          % [m/s^2]
-            % x = [ zQTMi(i); (zQTMi(i) - zQTMi(i-1))/(dt) ];
-        else
-            % dt = curTime - prevTime;                   % [seconds]
-            % u = ( zQTM(i) - zQTM(prevIter) ) / (dt^2); % [m/s^2]
-        end
+    % Update Input: Simulated Acceleration
+    u = (zQTMi(i) - zQTMi(i-1))/(dt^2);          % [m/s^2]
 
-        u = (zQTMi(i) - zQTMi(i-1))/(dt^2);          % [m/s^2]
-
-        % Current iteration variables
-        % dt = time(i+1)-time(i);
-        % y = zCV0(i);
-
-        deltaTime(i) = dt;
-        acc(i) = u;
-        
-        % Measurement Update - Update
-        Kk = (P*C')/(C*P*C' + R);
-        x = x + Kk*(y - C*x);
-        P = (I - Kk*C)*P;
+    deltaTime(i) = dt;
+    acc(i) = u;
     
-        % Extract Filtered Value of position
-        yEstimated = C*x;
-        zKalman(i) = yEstimated;
+    % Measurement Update - Update
+    Kk = (P*C')/(C*P*C' + R);
+    x = x + Kk*(y - C*x);
+    P = (I - Kk*C)*P;
 
-        % Time Update - Prediction
-        x = A*x + B*u;
-        P = A*P*A' + B*Q*B';
-    
-        % Add points to list
-        xKFestimate(i) = yEstimated;
+    % Extract Filtered Value of position
+    yEstimated = C*x;
+    zKalman(i) = yEstimated;
 
-        % iter = iter + 1;
-        % prevTime = curTime;
-        prevIter = i;
-        yPrev = y;
-    % end
+    % Time Update - Prediction
+    x = A*x + B*u;
+    P = A*P*A' + B*Q*B';
+
+    % Previous measurement if not detected
+    yPrev = y;
 end
 
+% Plot Kalman Filter Estimate
 plot(t, zKalman, '.r', MarkerSize=1)
+
 
 [a, icons] = legend('zCV0', 'zQTMinterp', 'zKalman', 'Location','eastoutside');
 % Change size of legend icons
