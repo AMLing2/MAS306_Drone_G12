2d38e45743104625a05a214262e666987e1fba08
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2024-05-08 11:29:51 +0200

    Removed cameraCenter unused lines, cleaner code.

--- Code/QualisysTest/cameraCenter.py
+++ Code/QualisysTest/cameraCenter.py
@@ -1,27 +1,22 @@
 # --------------------------------------- Libraries ---------------------------------------
 import cv2
-import cv2.aruco as aruco # For simplification
 import pyrealsense2 as rs
 import numpy
 # --------------------------------------- Libraries ---------------------------------------
 
 screenHeight = 480   # pixels
 screenWidth = 848    # pixels
+fps = 60             # frames/second
 cColor = (0, 0, 120) # bgr
 cThick = 1           # pixels
-
-# Set dictionary for the markers
-arucoParams     = aruco.DetectorParameters()
-arucoDictionary = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
 
 # Setup configuration and start pipeline stream
 pipe = rs.pipeline()
 config = rs.config()
 
-config.enable_stream(rs.stream.color, screenWidth, screenHeight, rs.format.bgr8, 60) # (streamType, xRes, yRes, format, fps)
+config.enable_stream(rs.stream.color, screenWidth, screenHeight, rs.format.bgr8, fps) # (streamType, xRes, yRes, format, fps)
 pipe.start(config)
 
-transVector = [0, 0, 0]
 while(True):
     
     # Frame Collection
@@ -30,8 +25,8 @@ while(True):
     color_image = numpy.asanyarray(color_frame.get_data()) # Convert to NumPy array
 
     # Display crosshair horizontal then vertical        
-    color_image = cv2.line(color_image, (0, int(screenHeight/2)), (screenWidth, int(screenHeight/2)), cColor, cThick)
-    color_image = cv2.line(color_image, (int(screenWidth/2), 0), (int(screenWidth/2), screenHeight,), cColor, cThick)
+    cv2.line(color_image, (0, int(screenHeight/2)), (screenWidth, int(screenHeight/2)), cColor, cThick)
+    cv2.line(color_image, (int(screenWidth/2), 0), (int(screenWidth/2), screenHeight,), cColor, cThick)
 
     # Display image
     cv2.imshow('LiveReading', color_image)
