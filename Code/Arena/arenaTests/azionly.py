d6b53de0070e98c58542746990f14ac3b9665dca
Adrian <amling@uia.no>
2024-05-18 21:07:26 +0200

    added motor and button test codes

--- Code/Arena/arenaTests/azionly.py
+++ Code/Arena/arenaTests/azionly.py
@@ -1,4 +1,3 @@
-#Martin Økter
 #Main controll of mid actuatror in the Drone Arena
 # 10/2/2022
 
@@ -14,71 +13,6 @@ GPIO.setwarnings(False)
 
 y_list = [0]
 x_list = [0]
-
-#Class setup
-class stepper_motor:
-    def __init__(self, direc, step, EN_pin, mode, base_wait_time, stepPerRot, roundUpDown):
-        self.direc_pin = direc
-        self.step_pin = step
-				#self.EN_pin = EN_pin
-        self.mode = mode
-        self.base_wait_time = base_wait_time
-        self.stepPerRot = stepPerRot
-        self.roundUpDown = roundUpDown
-        
-        self.resolution = {'Full':(0,0,0), 'Half':(1,0,0), '1/4':(0,1,0), '1/8':(1,1,0), '1/16':(0,0,1), '1/32':(1,0,1)}
-        self.resolution_multiplier = {'Full':1, 'Half':2, '1/4':4, '1/8':8, '1/16':16, '1/32':32}
-        self.resolution_choice = '1/16' #Best movement for the stepper in use
-        
-        GPIO.setup(self.direc_pin, GPIO.OUT)
-        GPIO.setup(self.step_pin, GPIO.OUT)
-        #GPIO.setup(self.EN_pin, GPIO.OUT)
-        GPIO.setup(self.mode, GPIO.OUT)
-        
-        #Stadard pins set
-				#GPIO.output(EN_pin, GPIO.LOW)
-        for i in range(3):
-            GPIO.output(self.mode, self.resolution[self.resolution_choice]) 
-        
-    def go_rounds(self, num_of_rounds, direction):
-        '''
-        Make the stepper go compete rounds for basic manuvering
-        with more easy to use number
-        May handle float number
-        '''
-        #Determine direction
-        if direction == 'Up':
-            GPIO.output(self.direc_pin, GPIO.LOW)
-        elif direction == 'Down':
-            GPIO.output(self.direc_pin, GPIO.HIGH)
-        
-        mode_multi = self.resolution_multiplier[self.resolution_choice]
-        adjuster = 10
-        
-        #Calculate number of steps
-        number_of_steps = round(num_of_rounds*self.stepPerRot*mode_multi)
-        
-        #Makes steps
-        for x in range(number_of_steps):
-                GPIO.output(self.step_pin, GPIO.HIGH)
-                time.sleep(self.base_wait_time/(mode_multi*adjuster))
-                GPIO.output(self.step_pin, GPIO.LOW)
-                time.sleep(self.base_wait_time/(mode_multi*adjuster))
-        
-    def go_up(self):
-        self.go_rounds(roundUpDown,'Up') #Make the piston move up max
-
-    def go_down(self):
-        self.go_rounds(roundUpDown,'Down') #Make the piston move down max
-    
-    def set_resolution(self, new_resolution):
-        '''
-        Set a new reutution of the stepper adjusting between microstepping and full
-        '''
-        self.resolution_choice = new_resolution
-        for i in range(3):
-            GPIO.output(self.mode, self.resolution[self.resolution_choice])
-        
 
 class pwm_motor:
     def __init__(self, motor_pin, freq, C1_pin, C2_pin, AIN1_pin, AIN2_pin):
@@ -199,6 +133,12 @@ class pwm_motor:
         
         #print(time.time())
 
+    def runPWM(self):
+        GPIO.output(self.AIN1, GPIO.HIGH)
+        GPIO.output(self.AIN2, GPIO.LOW)
+        self.motor.ChangeDutyCycle(30)
+        time.sleep(1)
+
 #Function setup
 def signal_handler(sig, frame):
     global y_list
@@ -214,16 +154,14 @@ def signal_handler(sig, frame):
    
 #Initialize motors
 #Initial basetime 0.0208
-mainStepper = stepper_motor(22,23,24,(8,7,1),0.0416,200,2.8)
 mainRotMot = pwm_motor(12,2000,25,16,27,17)
 
 #mainStepper.go_rounds(1,'Down','Up',1234567890)
 while True:
     print('Initiate run')
-    mainStepper.set_resolution('1/16')
-    mainStepper.go_rounds(0.3,'Down')
+    mainRotMot.runPWM()
     print('Run compleat')
-    #mainRotMot.go_to_pos(1)
+    mainRotMot.pwm_stop()
     #signal.signal(signal.SIGINT, signal_handler)
     #signal.pause()
     time.sleep(10)
